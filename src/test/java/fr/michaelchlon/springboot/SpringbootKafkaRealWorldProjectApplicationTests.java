fc477032ab8f19f84e0beb14cf11fb75df8bac3a
Michaël CHLON <mic.a.elle.chlon@gmail.com>
2025-06-10 12:53:41 +0200

    🚀 Initial commit

---
+++ src/test/java/fr/michaelchlon/springboot/SpringbootKafkaRealWorldProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.michaelchlon.springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootKafkaRealWorldProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
